f7f78034ca12754f46a25d6fea0902c3796e5e9b
Dieison Lemes Vieira <94806311+Dieison37@users.noreply.github.com>
2022-04-24 10:44:12 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<'GenreResponseProps'>({} as 'GenreResponseProps');
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar selectedGenreId={selectedGenreId} handleClickButton={handleClickButton} />
+      <Content selectedGenre = {selectedGenre} selectedGenreId = {selectedGenreId} />
+    </div>
+  )
+}
---
+++ src/components/Content.tsx
@@ -0,0 +1,48 @@
+import { SetStateAction, useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  mdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface ContentProps {
+  selectedGenreId: number;
+  selectedGenre: GenreResponseProps;
+}
+
+export function Content({selectedGenreId, selectedGenre}: ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then((response: { data: SetStateAction<MovieProps[]>; }) => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId]);
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.mdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
+}
